02f89f00d64f74e411971086d59eb400ef1dc67a
skredenmathias <skredenmathias@users.noreply.github.com>
2020-11-17 18:53:47 +0100

    unsure what this commit is.

--- pages/index.py
+++ pages/index.py
@@ -26,7 +26,7 @@ column1 = dbc.Col(
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/insights')
+        dcc.Link(dbc.Button('Analysis Summary', color='primary'), href='/insights')
     ],
     md=4,
 )
