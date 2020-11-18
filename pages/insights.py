1a8b2455915cba66ba4d28394d2fce56e54474d7
skredenmathias <skredenmathias@users.noreply.github.com>
2020-11-18 08:50:28 +0100

    website functional. Uncommented nested rows, layout still commented

--- pages/insights.py
+++ pages/insights.py
@@ -32,108 +32,108 @@ wind_park_decision_tree_feature_importances2 = html.Img(src='assets/wind_park_de
 wind_park_anomalies2 = html.Img(src='assets/wind_park_anomalies2.png', className='img-fluid')
 
 
-# header = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
-#             ## Insights  
-#             Prosjektet startet med SimpleLog.bak som ble lastet opp til en FTP server tilhørende Markedskraft. Med et Python SFTP library koblet jeg meg opp mot serveren og lastet ned SimpleLog filen. Dette var en ukjent filtype, og det var krevende å få lastet inn data med kjente teknikker. Hverken Microsoft Server Management Studio (SMSS), PostgreSQL, eller Python gjorde jobben.
+header = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+            ## Insights  
+            Prosjektet startet med SimpleLog.bak som ble lastet opp til en FTP server tilhørende Markedskraft. Med et Python SFTP library koblet jeg meg opp mot serveren og lastet ned SimpleLog filen. Dette var en ukjent filtype, og det var krevende å få lastet inn data med kjente teknikker. Hverken Microsoft Server Management Studio (SMSS), PostgreSQL, eller Python gjorde jobben.
 
-#             Jeg valgte å sende en mail til børre, da jeg ikke selv greide å finne problemet. I denne perioden hadde jeg enda ikke fått tilgang til dataen, så jeg begynte arbeidet med å sette opp en Plotly Dash nettside med CI via Travis CI og CD gjennom Heroku. Jeg satt også opp min egen database med et Docker mssql image. Etter kontakt med Birger, fant Børre og jeg en løsning med Microsoft SMSS. 
+            Jeg valgte å sende en mail til børre, da jeg ikke selv greide å finne problemet. I denne perioden hadde jeg enda ikke fått tilgang til dataen, så jeg begynte arbeidet med å sette opp en Plotly Dash nettside med CI via Travis CI og CD gjennom Heroku. Jeg satt også opp min egen database med et Docker mssql image. Etter kontakt med Birger, fant Børre og jeg en løsning med Microsoft SMSS. 
 
-#             Dataen var da analysert med SQL queries i SMSS, Etter å ha kikket på dataene, virket det på meg som at dbo.LogLevel og dbo.Applications kun var inkludert fordi dbo.Log benyttet en kolonne fra hver. Jeg konsentrerte derfor videre fokus rundt dbo.Log. Dette både reduserte kompleksitet og tid det tok meg å vaske datasettet. Uten å gå glipp av potensielt viktige data. 
-#             """
-#         ),
-#     ],
-#     width=12,
-# )
+            Dataen var da analysert med SQL queries i SMSS, Etter å ha kikket på dataene, virket det på meg som at dbo.LogLevel og dbo.Applications kun var inkludert fordi dbo.Log benyttet en kolonne fra hver. Jeg konsentrerte derfor videre fokus rundt dbo.Log. Dette både reduserte kompleksitet og tid det tok meg å vaske datasettet. Uten å gå glipp av potensielt viktige data. 
+            """
+        ),
+    ],
+    width=12,
+)
 
-# block4 = dbc.Col(
-#     [ 
-#         html.Div(
-#             initial_df   
-#         )  
-#     ]
-# )
+block4 = dbc.Col(
+    [ 
+        html.Div(
+            initial_df   
+        )  
+    ]
+)
 
-# block5 = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
-#             Dataen ble i Python omgjort til datetime format, sortert, og analysert i kronologisk rekkefølge. Jeg gikk nøye gjennom hver applikasjon, og gjorde notater for å kartlegge deres funksjonalitet, data, kvalitet og sammenheng med resterende programmer. 
+block5 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+            Dataen ble i Python omgjort til datetime format, sortert, og analysert i kronologisk rekkefølge. Jeg gikk nøye gjennom hver applikasjon, og gjorde notater for å kartlegge deres funksjonalitet, data, kvalitet og sammenheng med resterende programmer. 
 
-#             Når denne listen var komplett, satt jeg igjen med en tilstrekkelig forståelse av datasettet jeg hadde levert. Jeg gjorde meg en rekke betraktninger rundt datasettet: 
+            Når denne listen var komplett, satt jeg igjen med en tilstrekkelig forståelse av datasettet jeg hadde levert. Jeg gjorde meg en rekke betraktninger rundt datasettet: 
 
-#             """
-#         ),
-#     ],
-#     md=4,
-# )
+            """
+        ),
+    ],
+    md=4,
+)
 
-# block6 = dbc.Col(
-#     [ 
-#         html.Div(
-#             nulls_pricecalc   
-#         )  
-#     ]
-# )
+block6 = dbc.Col(
+    [ 
+        html.Div(
+            nulls_pricecalc   
+        )  
+    ]
+)
 
-# block7 = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
-#             Jeg valgte å videre analysere tre applikasjoner:
-#             RockyII
-#             Index_Mgmt
-#             Wind_Power_Import
-#             Da disse så ut til å holde mest relevant data. Disse applikasjonene hadde også mer variert data, som lett kunne benyttes uten mye skrubbing.
+block7 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+            Jeg valgte å videre analysere tre applikasjoner:
+            RockyII
+            Index_Mgmt
+            Wind_Power_Import
+            Da disse så ut til å holde mest relevant data. Disse applikasjonene hadde også mer variert data, som lett kunne benyttes uten mye skrubbing.
             
-#             """
-#         ),
-#     ],
-#     md=4,
-# )
+            """
+        ),
+    ],
+    md=4,
+)
 
-# block8 = dbc.Col(
-#     [
-#         html.Img(id='img4', src=url_img4, width="100%")      
-#     ]
-# )
+block8 = dbc.Col(
+    [
+        html.Img(id='img4', src=url_img4, width="100%")      
+    ]
+)
 
-# block9 = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
-#             Jeg skrev funksjoner for å sortere tekst-dataen og formatere den i håndterlige tabeller. Analysen fokuserte hovedsakelig på data fra svenske vindparker (Wind_Power_Import). Jeg kjørte en anomaly modell og lagde en decision tree modell som predikerte strømproduksjon basert på temperatur, dato, og vindpark. Jeg fant blant annet ut at
-#             Kulltorp returnerer kun NULL verdier.
-#             Bleikevare produserer betydelig mye mer strøm enn de andre parkene.
-#             Dagene lekker data inn i modellen.
-#             Dette er fordi datasettet ble splittet slik at noen av de samme dagene ble inkludert i trenings og test settet.
-#             Dermed har modellen lært seg en korrelasjon som gjør at den legger høyere vekt på visse dager hvor det var høy vindhastighet og produksjon. Denne korrelasjonen er ikke reell, da det er kunstig mange variabler som er berørt av denne “dobbel-lagringen”. 
-#             Modellen kan for eksempel forbedres med å kjøre en random forest modell på mer data. Jeg tror modellen forbedres betraktelig med data fra flere vindparker, barometrisk trykk og forbruksdata ol. fra SE3, SE4 og potensielt andre relevante prisområder.
+block9 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+            Jeg skrev funksjoner for å sortere tekst-dataen og formatere den i håndterlige tabeller. Analysen fokuserte hovedsakelig på data fra svenske vindparker (Wind_Power_Import). Jeg kjørte en anomaly modell og lagde en decision tree modell som predikerte strømproduksjon basert på temperatur, dato, og vindpark. Jeg fant blant annet ut at
+            Kulltorp returnerer kun NULL verdier.
+            Bleikevare produserer betydelig mye mer strøm enn de andre parkene.
+            Dagene lekker data inn i modellen.
+            Dette er fordi datasettet ble splittet slik at noen av de samme dagene ble inkludert i trenings og test settet.
+            Dermed har modellen lært seg en korrelasjon som gjør at den legger høyere vekt på visse dager hvor det var høy vindhastighet og produksjon. Denne korrelasjonen er ikke reell, da det er kunstig mange variabler som er berørt av denne “dobbel-lagringen”. 
+            Modellen kan for eksempel forbedres med å kjøre en random forest modell på mer data. Jeg tror modellen forbedres betraktelig med data fra flere vindparker, barometrisk trykk og forbruksdata ol. fra SE3, SE4 og potensielt andre relevante prisområder.
 
-#             """
-#         ),
-#     ],
-#     md=4,
-# )
+            """
+        ),
+    ],
+    md=4,
+)
 
-# block10 = dbc.Col(
-#     [
-#         html.Img(id='img3', src=url_img3, width="100%")      
-#     ]
-# )
+block10 = dbc.Col(
+    [
+        html.Img(id='img3', src=url_img3, width="100%")      
+    ]
+)
 
-# blankrow = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
-#             &nbsp;
-#             """
-#         ),
-#     ],
-#     md=4,
-# )
+blankrow = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+            &nbsp;
+            """
+        ),
+    ],
+    md=4,
+)
 # layout = dbc.Col([
 #     dbc.Row(header),
 #     dbc.Row([block4])
